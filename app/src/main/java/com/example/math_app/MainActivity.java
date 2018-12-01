8e1961057cb0966f346e50bea12672843a92d8cc
vladb_d <vladislav.96@mail.ru>
2018-12-01 16:46:47 +0200

    search bar

--- app/src/main/java/com/example/math_app/MainActivity.java
+++ app/src/main/java/com/example/math_app/MainActivity.java
@@ -12,18 +12,26 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
-import android.widget.SearchView;
+import android.widget.ListView;
+//import android.widget.SearchView;
+
 import android.widget.Toast;
 
 import com.example.math_app.fragments.Fragment_BugReport;
@@ -226,6 +234,8 @@ import com.example.math_app.fragments.klass_9.Fragment_Klass_9_8;
 import com.example.math_app.fragments.klass_9.Fragment_Klass_9_9;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -871,30 +881,63 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+         /*
+https://www.dev2qa.com/android-actionbar-searchview-autocomplete-example/
+        */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         getMenuInflater().inflate(R.menu.bug_report, menu);
+        getMenuInflater().inflate(R.menu.material_search_menu, menu);
 
-        MenuItem searcItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) searcItem.getActionView();
-        
 
-        searchView.setOnQueryTextListener(  new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
-                        System.out.println("onQueryTextSubmitonQueryTextSubmitonQueryTextSubmitonQueryTextSubmit");
-                        return false;
-                    }
+        // Inflate the search menu action bar.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.material_search_menu, menu);
 
-                    @Override
-                    public boolean onQueryTextChange(String newText) {
-                        System.out.println(newText+" onQueryTextChangeonQueryTextChangeonQueryTextChange");
-                        return false;
-                    }
-                }
-        );
+        // Get the search menu.
+        MenuItem searchMenu = menu.findItem(R.id.app_bar_menu_search);
+
+        // Get SearchView object.
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenu);
+
+        // Get SearchView autocomplete object.
+        final SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchAutoComplete.setBackgroundColor(Color.BLUE);
+        searchAutoComplete.setTextColor(Color.GREEN);
+        searchAutoComplete.setDropDownBackgroundResource(android.R.color.holo_blue_light);
+
+        // Create a new ArrayAdapter and add data to search auto complete object.
+        String dataArr[] = {"Apple" , "Amazon" , "Amd", "Microsoft", "Microwave", "MicroNews", "Intel", "Intelligence"};
+        ArrayAdapter<String> newsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dataArr);
+        searchAutoComplete.setAdapter(newsAdapter);
+
+        // Listen to search view item on click event.
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long id) {
+                String queryString=(String)adapterView.getItemAtPosition(itemIndex);
+                searchAutoComplete.setText("" + queryString);
+                Toast.makeText(MainActivity.this, "you clicked " + queryString, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Below event is triggered when submit search query.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setMessage("Search keyword is " + query);
+                alertDialog.show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
 
         return true;
@@ -906,7 +949,7 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_search) {
+        if (id == R.id.app_bar_menu_search) {
             /*
             new SimpleSearchDialogCompat(MainActivity.this, "Otsi...", "Sisestage märksõna",
                     null, initData(), new SearchResultListener<Searchable>() {
